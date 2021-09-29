2a7f6749fede87842ae78fe7ef496f59491ab8c9
OmkarBhede <omkarbhede@gmail.com>
2021-09-29 19:06:03 +0530

    my resume

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Document, Page } from "react-pdf";
+import page1 from "./0001.jpg";
+import page2 from "./0002.jpg";
+import resume from "./resume.pdf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="bg-gray-400 md:px-24 pt-2 md:pt-10">
+      <div className="md:mx-24 shadow-7xl mx-2  md:pb-10 pb-4">
+        <img className="rounded-t-3xl " src={page1}></img>
+        <img className="rounded-b-3xl" src={page2}></img>
+      </div>
+      <div className="flex items-center justify-center md:pb-10 pb-4">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={resume}
+          download
           target="_blank"
-          rel="noopener noreferrer"
+          className=" flex items-center bg-purple-600 hover:bg-purple-700 rounded-lg shadow-xl  md:text-lg text-white px-4 py-2"
         >
-          Learn React
+          Download Resume
         </a>
-      </header>
+      </div>
     </div>
   );
 }
